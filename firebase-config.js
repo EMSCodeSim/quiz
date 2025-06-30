02cc66f77a85bb27985569f42eb0b7723e22ad85
EMSCodeSim <davidagallaher@hotmail.com>
2025-06-30 04:54:20 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,22 @@
-// Replace values with your own from Firebase Console > Project Settings > Web App
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  databaseURL: "https://your-project.firebaseio.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDM6DpRSeZueVKRpbyJyDmhf8WY66KyCDk",
+  authDomain: "dailyquiz-d5279.firebaseapp.com",
+  databaseURL: "https://dailyquiz-d5279-default-rtdb.firebaseio.com",
+  projectId: "dailyquiz-d5279",
+  storageBucket: "dailyquiz-d5279.firebasestorage.app",
+  messagingSenderId: "94577748034",
+  appId: "1:94577748034:web:c032d3a1d72db1313de5db",
+  measurementId: "G-19DVN7NNH7"
 };
 
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
