6addda54987ef8ca362a294a4dfde3ee3e4bbc54
EMSCodeSim <davidagallaher@hotmail.com>
2025-06-30 05:01:56 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,31 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// ONLY use this if you're using <script src="https://www.gstatic.com/firebasejs/..."> in HTML
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDM6DpRSeZueVKRpbyJyDmhf8WY66KyCDk",
   authDomain: "dailyquiz-d5279.firebaseapp.com",
   databaseURL: "https://dailyquiz-d5279-default-rtdb.firebaseio.com",
   projectId: "dailyquiz-d5279",
-  storageBucket: "dailyquiz-d5279.firebasestorage.app",
+  storageBucket: "dailyquiz-d5279.appspot.com",
   messagingSenderId: "94577748034",
   appId: "1:94577748034:web:c032d3a1d72db1313de5db",
   measurementId: "G-19DVN7NNH7"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
-  firebase.database().ref(".info/connected").on("value", (snap) => {
+// Optional: test connection
+firebase.database().ref(".info/connected").on("value", (snap) => {
   if (snap.val() === true) {
     console.log("✅ Connected to Firebase");
   } else {
     console.error("❌ Not connected to Firebase");
   }
 });
-
